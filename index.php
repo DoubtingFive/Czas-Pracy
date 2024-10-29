80b7af2e4e6d7bd17a3b7d371e78dd9449dd29bd
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-29 14:32:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,21 +19,21 @@
     <main>
         <h2>Lista Pracowników i Czas Pracy</h2>
         <?php
-        $idp = mysqli_connect("localhost","website","mySmGZ@04d5*J85o","pracownicy");
-        
-        $idd = mysqli_query($idp,"SELECT * FROM pracownicy");
-        $code = "<table><tr><th>
-                Imie
-                </th><th>
-                Nazwisko
-                </th><th>
-                Godziny pracy
-                </th></tr></table";
-        while ($row= mysqli_fetch_row($idd)) {
-          $code .= "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
-        }
-        $code .= "</table>";
-        echo $code;
+            $idp = mysqli_connect("localhost","website","mySmGZ@04d5*J85o","pracownicy");
+            
+            $idd = mysqli_query($idp,"SELECT * FROM pracownicy");
+            $code = "<table><tr><th>
+                    Imie
+                    </th><th>
+                    Nazwisko
+                    </th><th>
+                    Godziny pracy
+                    </th></tr></table";
+            while ($row= mysqli_fetch_row($idd)) {
+              $code .= "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
+            }
+            $code .= "</table>";
+            echo $code;
         ?>
     </main>
 </body>
