3166b9fa91695ba2d5e7ba35f78cfc0bd36f7ccd
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-30 13:45:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -74,22 +74,7 @@ $x = $_SESSION['start_sesji'];
 		</script>
 
         <!-- Tabela -->
-		<?php
-			$idp = mysqli_connect("localhost","website","mySmGZ@04d5*J85o","pracownicy") or die("Nie udalo sie polaczyc z baza danych pracownicy");
-
-			$idd = mysqli_query($idp,"SELECT * FROM pracownicy");
-			$code = "<table><tr><th>
-				ID</th><th>
-				Imie</th><th>
-				Nazwisko</th><th>
-				Godziny pracy</th></tr>";
-			while ($row= mysqli_fetch_row($idd)) {
-				$code .= "<tr><td>$row[3]</td><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
-			}
-			$code .= "</table>";
-			echo $code;
-			mysqli_close($idp);
-		?>
+		<?php include 'php/tabela.php';?>
     </main>
 </body>
 </html>
