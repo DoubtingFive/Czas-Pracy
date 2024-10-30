ed9ac1c9a882638e6333cec8d7780ce21ee8eec0
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-30 13:56:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,6 @@ $x = $_SESSION['start_sesji'];
         <h2>Lista Pracowników i Czas Pracy</h2>
         <!-- Liczenie czasu -->
 		<script>
-			var d = new Date();
 			const startCzasu = <?php echo $x; ?>;
 		</script>
 		<script src="javascript/licznikCzasu.js"></script>
