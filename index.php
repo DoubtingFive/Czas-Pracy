7ac702338bf4493df963905422fa0f2521f468d3
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-30 09:05:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,14 +23,14 @@
             
             $idd = mysqli_query($idp,"SELECT * FROM pracownicy");
             $code = "<table><tr><th>
-              ID
-            </th><th>
-              Imie
-            </th><th>
-              Nazwisko
-            </th><th>
-              Godziny pracy
-            </th></tr></table";
+					ID
+				</th><th>
+					Imie
+				</th><th>
+					Nazwisko
+				</th><th>
+					Godziny pracy
+				</th></tr></table";
             while ($row= mysqli_fetch_row($idd)) {
               $code .= "<tr><td>$row[3]</td><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
             }
