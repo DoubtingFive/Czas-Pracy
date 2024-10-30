f6555135e06fb5e2492175086f08b9da7d461b85
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-30 13:42:30 +0100

    Create tabela.php

---
+++ php/tabela.php
@@ -0,0 +1,16 @@
+<?php
+			$idp = mysqli_connect("localhost","website","mySmGZ@04d5*J85o","pracownicy") or die("Nie udalo sie polaczyc z baza danych pracownicy");
+
+			$idd = mysqli_query($idp,"SELECT * FROM pracownicy");
+			$code = "<table><tr><th>
+				ID</th><th>
+				Imie</th><th>
+				Nazwisko</th><th>
+				Godziny pracy</th></tr>";
+			while ($row= mysqli_fetch_row($idd)) {
+				$code .= "<tr><td>$row[3]</td><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
+			}
+			$code .= "</table>";
+			echo $code;
+			mysqli_close($idp);
+		?>
