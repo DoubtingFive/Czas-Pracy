fe5b830e1a8256bd6437f6adc760030c4280332c
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-30 11:39:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+if (!isset($_SESSION["Login"])) {
+	header("Location: login.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -7,6 +13,8 @@
     <title>Czas Pracy Pracowników</title>
 </head>
 <body>
+	<p style="position:absolute;" id="czas">Czas sesji: 00:00</p>
+	<button id="logout" style="position:absolute;right:0px;" onclick="Logout();"></button>
     <header>
         <h1>Czas Pracy Pracowników</h1>
     </header>
@@ -16,27 +24,58 @@
         <a href="#raporty">Raporty</a>
         <a href="#ustawienia">Ustawienia</a>
     </nav>
-    <main>
+	<main>
         <h2>Lista Pracowników i Czas Pracy</h2>
-        <?php
-            $idp = mysqli_connect("localhost","website","mySmGZ@04d5*J85o","pracownicy");
-            
-            $idd = mysqli_query($idp,"SELECT * FROM pracownicy");
-            $code = "<table><tr><th>
-					ID
-				</th><th>
-					Imie
-				</th><th>
-					Nazwisko
-				</th><th>
-					Godziny pracy
-				</th></tr></table";
-            while ($row= mysqli_fetch_row($idd)) {
-              $code .= "<tr><td>$row[3]</td><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
-            }
-            $code .= "</table>";
-            echo $code;
-        ?>
+        <!-- Liczenie czasu -->
+		<script>
+			czas = 0;
+			czasNieaktywnosci = 0;
+			const czasObj = document.getElementById("czas");
+			setInterval(function() {
+				// nieaktywnosc
+				czasNieaktywnosci++;
+				if (czasNieaktywnosci >= 600) {
+					// log out
+				}
+
+				// czas
+				czas++;
+				h = Math.floor(czas/60/60);
+				m = Math.floor(czas/60%60);
+				s = czas%60;
+				czasObj.innerHTML = "Czas sesji: "+ 
+				((h >= 1)?h+":":"") +
+				((m < 10)?(m == 0)?"00":"0"+m:m) + ":" + 
+				((s < 10)?(s == 0)?"00":"0"+s:s);
+			}, 1000);
+
+			document.addEventListener("mousemove",ResetNieaktywnosci)
+			function ResetNieaktywnosci() {
+				czasNieaktywnosci = 0;
+			}
+			
+			function Logout() {
+				location.replace("logout.php");
+			}
+		</script>
+
+        <!-- Tabela -->
+		<?php
+			$idp = mysqli_connect("localhost","website","mySmGZ@04d5*J85o","pracownicy") or die("Nie udalo sie polaczyc z baza danych pracownicy");
+
+			$idd = mysqli_query($idp,"SELECT * FROM pracownicy");
+			$code = "<table><tr><th>
+				ID</th><th>
+				Imie</th><th>
+				Nazwisko</th><th>
+				Godziny pracy</th></tr></table>";
+			while ($row= mysqli_fetch_row($idd)) {
+				$code .= "<tr><td>$row[3]</td><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
+			}
+			$code .= "</table>";
+			echo $code;
+			mysqli_close($idp);
+		?>
     </main>
 </body>
 </html>
