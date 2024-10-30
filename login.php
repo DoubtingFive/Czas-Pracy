79af7bbfe45c222ee2ef8c1fedf022dcd28028de
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-30 12:02:16 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['login'], $_SESSION['czas'])) {
-	header("Location: login.php");
-}
 ?>
-
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -14,69 +10,36 @@ if (!isset($_SESSION['login'], $_SESSION['czas'])) {
     <title>Czas Pracy Pracowników</title>
 </head>
 <body>
-	<p style="position:absolute;" id="czas">Czas sesji: 00:00</p>
-	<button id="logout" style="position:absolute;right:0px;" onclick="Logout();">Wyloguj się</button>
     <header>
         <h1>Czas Pracy Pracowników</h1>
     </header>
-    <nav>
-        <a href="#strona_glowna">Strona Główna</a>
-        <a href="#pracownicy">Pracownicy</a>
-        <a href="#raporty">Raporty</a>
-        <a href="#ustawienia">Ustawienia</a>
-    </nav>
 	<main>
-        <h2>Lista Pracowników i Czas Pracy</h2>
-        <!-- Liczenie czasu -->
-		<script>
-			czas = <?php time() - $_SESSION['session_start_time']; ?>
-			const czasObj = document.getElementById("czas");
-			czasNieaktywnosci = 0;
-
-			setInterval(function() {
-				// nieaktywnosc
-				czasNieaktywnosci++;
-				if (czasNieaktywnosci >= 600) {
-					Logout();
-				}
-
-				// czas
-				czas++;
-				h = Math.floor(czas/60/60);
-				m = Math.floor(czas/60%60);
-				s = czas%60;
-				czasObj.innerHTML = "Czas sesji: "+ 
-				((h >= 1)?h+":":"") +
-				((m < 10)?(m == 0)?"00":"0"+m:m) + ":" + 
-				((s < 10)?(s == 0)?"00":"0"+s:s);
-			}, 1000);
-
-			document.addEventListener("mousemove",ResetNieaktywnosci)
-			function ResetNieaktywnosci() {
-				czasNieaktywnosci = 0;
-			}
-			
-			function Logout() {
-				location.replace("logout.php");
-			}
-		</script>
-
-        <!-- Tabela -->
+        <!-- Logowanie -->
 		<?php
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$idp = mysqli_connect("localhost","website","mySmGZ@04d5*J85o","pracownicy") or die("Nie udalo sie polaczyc z baza danych pracownicy");
 
-			$idd = mysqli_query($idp,"SELECT * FROM pracownicy");
-			$code = "<table><tr><th>
-				ID</th><th>
-				Imie</th><th>
-				Nazwisko</th><th>
-				Godziny pracy</th></tr>";
-			while ($row= mysqli_fetch_row($idd)) {
-				$code .= "<tr><td>$row[3]</td><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
-			}
-			$code .= "</table>";
-			echo $code;
+			$idd = mysqli_query($idp,"SELECT * FROM uzytkownicy WHERE Login='".$_POST['login']."' AND Haslo='".$_POST['pass']."';");
+			$user = mysqli_fetch_assoc($idd);
 			mysqli_close($idp);
+
+			if ($user) {
+				$_SESSION['login'] = $user['Login'];
+				header("Location:index.php");
+			} else{
+				echo "Nie istnieje taki użytkownik.";
+				NoLogin();
+			}
+		} else{
+			NoLogin();
+		}
+		function NoLogin() {
+			echo '<form action="login.php" method="POST">
+				<input type="text" name="login" id="login" required><br><br>
+				<input type="password" name="pass" id="pass" required><br><br>
+				<input type="submit" value="Zaloguj">
+			</form>';
+		}
 		?>
     </main>
 </body>
