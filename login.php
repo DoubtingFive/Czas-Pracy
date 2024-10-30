3696c4df1fc27c5cb66ff2e13dac39c3e5b92e30
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-30 13:38:53 +0100

    Update login.php

--- login.php
+++ login.php
@@ -15,33 +15,7 @@ session_start();
     </header>
 	<main>
         <!-- Logowanie -->
-		<?php
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			$idp = mysqli_connect("localhost","website","mySmGZ@04d5*J85o","pracownicy") or die("Nie udalo sie polaczyc z baza danych pracownicy");
-
-			$idd = mysqli_query($idp,"SELECT * FROM uzytkownicy WHERE Login='".$_POST['login']."' AND Haslo='".$_POST['pass']."';");
-			$user = mysqli_fetch_assoc($idd);
-			mysqli_close($idp);
-
-			if ($user) {
-				$_SESSION['login'] = $user['Login'];
-				$_SESSION['start_sesji'] = time();
-				header("Location:index.php");
-			} else{
-				echo "Nie istnieje taki użytkownik.";
-				NoLogin();
-			}
-		} else{
-			NoLogin();
-		}
-		function NoLogin() {
-			echo '<form action="login.php" method="POST">
-				<input type="text" name="login" id="login" required><br><br>
-				<input type="password" name="pass" id="pass" required><br><br>
-				<input type="submit" value="Zaloguj">
-			</form>';
-		}
-		?>
+	<?php include 'php/logowanie/zaloguj.php';?>
     </main>
 </body>
 </html>
