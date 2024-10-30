f6e093bd80acd5ee83c68b768d2146f01cf5d881
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-30 13:51:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -29,49 +29,7 @@ $x = $_SESSION['start_sesji'];
 	<main>
         <h2>Lista Pracowników i Czas Pracy</h2>
         <!-- Liczenie czasu -->
-		<script>
-			var d = new Date();
-			const startCzasu = <?php echo $x; ?>;
-			const czasObj = document.getElementById("czas");
-			czasNieaktywnosci = d.getTime();
-
-			LiczCzas();
-			const xddd = setInterval(LiczCzas, 1000);
-
-			function LiczCzas() {
-                d = new Date();
-                const czasTeraz = d.getTime();
-                const roznica = czasTeraz - czasNieaktywnosci;
-
-				// nieaktywnosc
-                console.log(`d.getTime(): ${d.getTime()} | czasNieaktywnosci: ${czasNieaktywnosci} | diff: ${roznica}`);
-				if (roznica >= 600*1000) {
-                    console.log(`nastepuje wylogowywanie`)
-					clearInterval(xddd);
-                    setTimeout(Logout,1000);
-				}
-
-				// czas
-				let czas = Math.floor(d.getTime()/1000) - startCzasu;
-				let h = Math.floor(czas/60/60);
-				let m = Math.floor(czas/60%60);
-				let s = czas%60;
-				czasObj.innerHTML = "Czas sesji: "+ 
-				((h >= 1)?h+":":"") +
-				((m < 10)?(m == 0)?"00":"0"+m:m) + ":" + 
-				((s < 10)?(s == 0)?"00":"0"+s:s);
-			}
-
-			document.addEventListener("mousemove",ResetNieaktywnosci)
-			function ResetNieaktywnosci() {
-                d = new Date();
-				czasNieaktywnosci = d.getTime();
-			}
-			
-			function Logout() {
-				location.replace("php/logowanie/wylogowanie.php");
-			}
-		</script>
+		<script src="javascript/licznikCzasu.js"></script>
 
         <!-- Tabela -->
 		<?php include 'php/tabela.php';?>
