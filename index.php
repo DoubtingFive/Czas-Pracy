38388ffbfcf0d9ea71578a4e8dd37450ee8473dd
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-31 09:55:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,32 +12,46 @@ $y = $_SESSION['login'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Czas pracy</title>
     <link rel="stylesheet" href="style/styl.css">
-    <title>Czas Pracy Pracowników</title>
 </head>
 <body>
-	<p style="position:absolute;" id="czas">Czas sesji: 00:00</p>
-	<button id="logout" style="position:absolute;right:0px;" onclick="Logout();">Wyloguj się</button>
-    <header>
-        <h1>Czas Pracy Pracowników</h1>
-    </header>
-    <nav>
-        <a href="#strona_glowna">Strona Główna</a>
-        <a href="#pracownicy">Pracownicy</a>
-        <a href="#raporty">Raporty</a>
-        <a href="#ustawienia">Ustawienia</a>
-    </nav>
-	<main>
-        <h2>Lista Pracowników i Czas Pracy</h2>
-        <!-- Liczenie czasu -->
-		<script>
-			const startCzasu = <?php echo $x; ?>;
-			const nazwaUzytkownika = "<?php echo $y; ?>";
-		</script>
-		<script src="javascript/licznikCzasu.js"></script>
-
-        <!-- Tabela -->
-		<?php include 'php/tabela.php';?>
-    </main>
+    <img src="" alt="logo">
+    <div id="calosc">
+        <div id="baner">
+            <h1 id="tytul">Czas pracy</h1>
+        </div>
+        <div id="nawigacja">
+			<fieldset>
+                <legend>Nawigacja</legend>
+                <br>
+                <a href="#strona_glowna">Strona Główna</a><br>
+                <br>
+                <a href="#pracownicy">Pracownicy</a><br>
+                <br>
+                <a href="#raporty">Raporty</a><br>
+                <br>
+                <a href="#ustawienia">Ustawienia</a><br>
+            </fieldset>
+        </div>
+        <div id="kontent">
+            <h2>Lista Pracowników i Czas Pracy</h2>
+            <!-- Liczenie czasu -->
+            <script>
+                const startCzasu = <?php echo $x; ?>;
+                const nazwaUzytkownika = "<?php echo $y; ?>";
+            </script>
+            <script src="javascript/licznikCzasu.js"></script>
+    
+            <!-- Tabela -->
+            <?php include 'php/tabela.php';?>
+        </div>
+        <div id="ustawienia">
+			<fieldset>
+                <legend>Ustawienia</legend>
+            </fieldset>
+        </div>
+        <div id="administracja"></div>
+    </div>
 </body>
 </html>
