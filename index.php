3331b79681c26da9e20797c9bf8b784cc3fb47e7
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-31 14:59:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -53,35 +53,7 @@ $y = $_SESSION['login'];
                 <legend>Ustawienia</legend>
             </fieldset>
         </div>
-        <div id="administracja"></div>
-
-        <div>
-            <!-- <?php include 'php/admin.php';?> -->
-            <button id="adminPanel" onclick="RozwinPanelAdmina(this);"></button>
-            <div id="administracja">
-                <form action="admin/rekord.php" method="POST">
-                    <label>Login</label>
-                    <input type="text" name="login" id="login" required>
-                    <label>Hasło</label>
-                    <input type="password" name="pass" id="pass" required>
-                    <input type="submit" value="Zaloguj">
-                </form>
-            </div>
-            <script>
-                const panel = document.getElementById('administracja');
-                const calosc = document.getElementById('calosc');
-                function RozwinPanelAdmina(x) {
-                    if (panel.style.bottom === '-30vh') {
-                        panel.style.bottom = '0';
-                        x.style.bottom = "30.5vh";
-                    } else {
-                        panel.style.bottom = '-30vh';
-                        x.style.bottom = ".5vh";
-                    }
-                }
-                RozwinPanelAdmina(document.getElementById("adminPanel"))
-            </script>
-        </div>
+        <?php include 'php/admin.php';?>
     </div>
 </body>
 </html>
