7ed31caa99d2944356a68d5523770800f6442c1c
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-31 11:05:17 +0100

    Update login.php

--- php/logowanie/login.php
+++ php/logowanie/login.php
@@ -6,8 +6,8 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/stylhtml.css">
-    <link rel="stylesheet" href="style/login.css">
+    <link rel="stylesheet" href="../../style/stylhtml.css">
+    <link rel="stylesheet" href="../../style/login.css">
     <title>Czas Pracy Pracowników</title>
 </head>
 <body>
