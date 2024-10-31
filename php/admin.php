6cda57e7a9c4e311043826bfc3991e21034dc5c9
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-31 15:43:09 +0100

    Add files via upload

---
+++ php/admin.php
@@ -0,0 +1,34 @@
+﻿<?php
+session_start();
+if (!isset($_SESSION['login'], $_SESSION['start_sesji'], $_SESSION['is_admin'])) {
+	header("Location: php/logowanie/login.php");
+}
+if ($_SESSION['isAdmin']) {
+    echo `
+    <div>
+        <button id="adminPanel" onclick="RozwinPanelAdmina(this);"></button>
+        <div id="administracja">
+            <form action="admin/rekord.php" method="POST">
+                <label>Login</label>
+                <input type="text" name="login" id="login" required>
+                <label>Hasło</label>
+                <input type="password" name="pass" id="pass" required>
+                <input type="submit" value="Zaloguj">
+            </form>
+        </div>
+        <script>
+            const panel = document.getElementById('administracja');
+            const calosc = document.getElementById('calosc');
+            function RozwinPanelAdmina(x) {
+                if (panel.style.bottom === '-30vh') {
+                    panel.style.bottom = '0';
+                    x.style.bottom = "30.5vh";
+                } else {
+                    panel.style.bottom = '-30vh';
+                    x.style.bottom = ".5vh";
+                }
+            }
+            RozwinPanelAdmina(document.getElementById("adminPanel"))
+        </script>
+    </div>`;
+}
