f4d46d402d6d8453eeb3cf46ffd585fab2051258
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-31 16:49:04 +0100

    Update admin.php

--- php/admin.php
+++ php/admin.php
@@ -1,9 +1,9 @@
 <?php
 if ((bool)$_SESSION['is_admin']) {
-    echo `
+    echo '
     <div>
-        <button id="adminPanel" onclick="RozwinPanelAdmina(this);"></button>
-        <div id="administracja">
+        <button id="adminPanel" onclick="RozwinPanelAdmina(this);">Panel Administracyjny</button>
+        <div id="administracja" style="position: fixed; bottom: -30vh; transition: bottom 0.3s;">
             <form action="admin/rekord.php" method="POST">
                 <label>Login</label>
                 <input type="text" name="login" id="login" required>
@@ -13,18 +13,16 @@ if ((bool)$_SESSION['is_admin']) {
             </form>
         </div>
         <script>
-            const panel = document.getElementById('administracja');
-            const calosc = document.getElementById('calosc');
-            function RozwinPanelAdmina(x) {
-                if (panel.style.bottom === '-30vh') {
-                    panel.style.bottom = '0';
-                    x.style.bottom = "30.5vh";
+            const panel = document.getElementById("administracja");
+            function RozwinPanelAdmina(button) {
+                if (panel.style.bottom === "-30vh") {
+                    panel.style.bottom = "0";
+                    button.style.bottom = "30.5vh"; // Move button when panel is shown
                 } else {
-                    panel.style.bottom = '-30vh';
-                    x.style.bottom = ".5vh";
+                    panel.style.bottom = "-30vh";
+                    button.style.bottom = ".5vh"; // Reset button position when hidden
                 }
             }
-            RozwinPanelAdmina(document.getElementById("adminPanel"))
         </script>
-    </div>`;
+    </div>';
 }
