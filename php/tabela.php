8b230fcfbea99f89d6dd5e51935e2b6d0c9a0f7b
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-11-05 10:36:45 +0100

    Update tabela.php

--- php/tabela.php
+++ php/tabela.php
@@ -1,16 +1,28 @@
 <?php
-	$idp = mysqli_connect("localhost","website","5mu4fDGv_Q58NbXV","pracownicy") or die("Nie udalo sie polaczyc z baza danych pracownicy");
+	$idp = mysqli_connect("localhost","website","5mu4fDGv_Q58NbXV","pracownicy")
+	or die("Nie udalo sie polaczyc z baza danych pracownicy");
 
-	$idd = mysqli_query($idp,"SELECT * FROM pracownicy");
+	$sql = "SELECT u.imie, u.nazwisko, w.data, w.godzina_rozpoczecia, w.godzina_zakonczenia, w.godzin, w.zatwierdzone FROM uzytkownicy AS u JOIN wpisy_pracy AS w ON w.uzytkownik_id=u.uzytkownik_id;";
+	$idd = mysqli_query($idp,$sql);
 	$code = "<table><tr><th>
-		ID</th><th>
 		Imie</th><th>
 		Nazwisko</th><th>
-		Godziny pracy</th></tr>";
+		Data</th></tr>
+		Godzina rozpoczęcia</th></tr>
+		Godzina zakończenia</th></tr>
+		Godzin pracy</th></tr>
+		Zatwierdzone</th></tr>";
 	while ($row= mysqli_fetch_row($idd)) {
-		$code .= "<tr><td>$row[3]</td><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
+		$code .= "<tr>
+		<td>$row[0]</td>
+		<td>$row[1]</td>
+		<td>$row[2]</td>
+		<td>$row[3]</td>
+		<td>$row[4]</td>
+		<td>$row[5]</td>
+		<td><span class='circle ".(($row[6])?"filled":"empty")."'></span></td>
+		</tr>";
 	}
 	$code .= "</table>";
 	echo $code;
 	mysqli_close($idp);
-?>
